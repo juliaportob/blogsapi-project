f98f74b6d21dea91e138fe8ec0a17b76d1724645
Júlia Pôrto <juportob@gmail.com>
2021-05-11 16:52:35 -0300

    feat: remove nodemodules and .env

--- controller/postController.js
+++ controller/postController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const { BlogPost, User } = require('../models');
 const { registerPost } = require('../middlewares/PostMid');
 const { verifyToken } = require('../middlewares/UserMid');
@@ -22,6 +23,32 @@ postRouter.get('/', verifyToken, async (_req, res) => {
     const posts = await BlogPost.findAll({
       include: [{ model: User, as: 'user' }],
     });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+postRouter.get('/search', verifyToken, async (req, res) => {
+  try {
+    const searchTerm = req.query.q;
+    const { email } = req.userData;
+    const user = await User.findOne({ where: { email } });
+    const idUser = user.dataValues.id;
+    const posts = await BlogPost.findAll({
+      include: { model: User, as: 'user', attributes: { exclude: 'password' } },
+      where: { idUser, [Op.or]: [
+        { title: { [Op.like]: `%${searchTerm}%` } },
+        { content: { [Op.like]: `${searchTerm}%` } },
+      ],
+      } });
+    if (searchTerm === '') {
+      const allPosts = await BlogPost.findAll({
+        include: { model: User, as: 'user', attributes: { exclude: 'password' } },
+        where: { userId: idUser },
+      });
+      return res.status(200).json(allPosts);
+    }
     return res.status(200).json(posts);
   } catch (error) {
     return res.status(500).json({ message: 'Algo deu errado' });
